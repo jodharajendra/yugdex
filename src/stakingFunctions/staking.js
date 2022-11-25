855acbaa52521e1df907b4b3e925e1f41c69ca10
Yash Mathur <65745613+theyashmathur@users.noreply.github.com>
2022-11-25 21:45:12 +0530

    stake functions

---
+++ src/stakingFunctions/staking.js
@@ -0,0 +1,49 @@
+import { ethers } from "ethers";
+import { address, abi } from "../config";
+
+const provider = new ethers.providers.Web3Provider(window.ethereum)
+const signer = provider.getSigner()
+
+const stakingAddress = address.stakeAddress
+const stakingAbi = abi.stakeAbi
+
+const tokenAddress = address.token
+const tokenAbi = abi.tokenAbi
+
+const token = new ethers.Contract(tokenAddress, tokenAbi, signer)
+const stake = new ethers.Contract(stakingAddress,stakingAbi, signer)
+
+export const checkAllowance = async () => {
+    const address = signer.getAddress()
+    let allowance = await token.allowance(address, stakingAddress)
+    allowance = parseInt(allowance, 10)
+    return allowance
+}
+
+export const approveStakingContract = async (amount) => {
+    const address = signer.getAddress()
+
+    let decimal = await token.decimals()
+    decimal = parseInt(decimal, 10)
+
+    amount = amount * 10 ** decimal
+    amountInHex = "0x" + amount.toString(16)
+
+    const tx = await token.approve(stakeAddress, amountInHex)
+    console.log("approve tx: ",tx)
+}
+
+export const stakeFunction = async (amount, poolId) => {
+    const address = signer.getAddress()
+
+    let decimal = await token.decimals()
+    decimal = parseInt(decimal, 10)
+
+    amount = amount * 10 ** decimal
+    amountInHex = "0x" + amount.toString(16)
+
+    poolInHex = "0x" + poolId.toString(16)
+
+    const tx = await stake.stakeTokens(amountInHex, poolInHex)
+    console.log("stake tx: ", tx)
+}
